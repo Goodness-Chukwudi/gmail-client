4406c50323d63936dcced76e7bb24622ab0ef344
Goodness-Chukwudi <ibechechukwudi@gmail.com>
2024-05-13 01:32:33 +0100

    expose email api endpoints

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,6 +1,7 @@
 import {Express} from "express";
 import Env from "../common/config/environment_variables";
 import AppController from "../controllers/AppController";
+import EmailController from "../controllers/EmailController";
 class AppRoutes {
 
     private app: Express;
@@ -11,6 +12,7 @@ class AppRoutes {
     initializeRoutes() {
         
         this.app.use(Env.API_PATH + "/", AppController);
+        this.app.use(Env.API_PATH + "/emails", EmailController);
     }
 }
 
